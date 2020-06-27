acb78e01734fe79fa2ec06434d4aa7a0b8740908
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-27 17:29:54 -0500

    App.js now succesfull writes to data.json with faker data

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+let fs = require('fs')
 let faker = require('faker')
-console.log(faker.name.findName())
+let data = require('./data/data.json')
+
+const names = new Array(5).fill('').map(elem => faker.name.findName())
+const namesobj = {
+    names: names
+}
+fs.writeFile('./data/data.json', JSON.stringify(namesobj), (err)=> {
+    console.log('write finished', err ? err : '')
+})
+/*
+fs.readFile('.data/data.json','utf-8', (err.data) => {
+    let data = JSON.parse(data)
+    console.log(data.names)
+})
+*/
