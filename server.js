6b0aacfd06d039514dc38909e1b27c22458cccb6
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-27 20:31:02 -0500

    Add code to post messages to /messages route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 app.use(express.static(__dirname))
+let messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'GoodBye'}
+]
+app.get('/messages', (req,res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req,res) => {
+    console.log(req.body)
+    res.sendStatus(200)
+})
+
 let server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
