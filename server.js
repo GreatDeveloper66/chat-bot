02fdb58f32a3470da46a2abb60f176dd8f34afb9
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-28 00:10:20 -0500

    Add body parser commands to server.js to link form input to nodejs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 let messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'GoodBye'}
