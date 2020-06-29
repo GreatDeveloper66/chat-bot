6c5ef1176408e96f75d83f5b56b27520ff634ec1
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-29 14:16:09 -0500

    server.js now succesfully connects to MongoDb Cluster

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-import express, { static } from 'express'
-import { json, urlencoded } from 'body-parser'
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-import mongoose from 'mongoose'
-app.use(static(__dirname))
-app.use(json())
-app.use(urlencoded({extended: false}))
+const mongoose = require('mongoose')
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+const dbUrl = 'mongodb+srv://chat-bot-user:OHv1RfoVJzi5DcPo@cluster0-vo8rx.mongodb.net/<dbname>?retryWrites=true&w=majority'
 let messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'GoodBye'}
@@ -25,6 +26,26 @@ io.on('connection', socket => {
     console.log('user connected')
 })
 
+mongoose.connect(dbUrl, { useNewUrlParser: true}, err => {
+    console.log('mongo db connection', err)
+})
+
 let server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
+
+
+/*
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://chat-bot-user:<password>@cluster0-vo8rx.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+*/
