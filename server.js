7bf45e3ad61a60e17aa43cea7de530114e3a83ff
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-29 13:17:24 -0500

    change require statements in server.js to conform to es6

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+import express, { static } from 'express'
+import { json, urlencoded } from 'body-parser'
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+import mongoose from 'mongoose'
+app.use(static(__dirname))
+app.use(json())
+app.use(urlencoded({extended: false}))
 let messages = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'GoodBye'}
